a354ca095c1cdabeb148f510b0ea94765c1b84bb
randyaajr <67880000+randyaajr@users.noreply.github.com>
2021-05-17 19:40:04 -0400

    Created portfolio section
    Must complete site addresses for the rest of my portfolio projects. **data.js**

---
+++ src/data.js
@@ -0,0 +1,19 @@
+export const featuredPortfolio = [{
+    id: 1,
+    title: "E-commerce",
+    img: "assets/boutique.PNG",
+    link: "https://randyaajr-shop.herokuapp.com/",
+}, ];
+
+export const webPortfolio = [{
+    id: 1,
+    title: "Love Runnings",
+    img: "assets/loverunnings0.PNG",
+    link: "https://randyaajr.github.io/Love-Runnings/",
+}, ];
+
+export const mobilePortfolio = [];
+
+export const gamesPortfolio = [];
+
+export const contentPortfolio = [];
