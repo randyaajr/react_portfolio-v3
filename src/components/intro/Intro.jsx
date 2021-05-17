0ad8c069c02bdc1515bcf89be882d9a2d6e28f4f
randyaajr <67880000+randyaajr@users.noreply.github.com>
2021-05-17 17:40:24 -0400

    Added the new renamed image path

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -17,7 +17,7 @@ export default function Intro() {
     <div className="intro" id="intro">
       <div className="left">
         <div className="imgContainer">
-          <img src="assets/me-1.png" alt="" />
+          <img src="assets/me.png" alt="" />
         </div>
       </div>
       <div className="right">
