3727cc7db9cc1f5b100c37b9cc8f436f4628a7cd
randyaajr <67880000+randyaajr@users.noreply.github.com>
2021-05-17 17:37:14 -0400

    Created the intro section

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,5 +1,37 @@
 import "./Intro.scss";
+import { init } from "ityped";
+import { useEffect, useRef } from "react";
 
 export default function Intro() {
-  return <div className="intro" id="intro"></div>;
+  const textRef = useRef();
+
+  useEffect(() => {
+    init(textRef.current, {
+      backDelay: 1500,
+      backSpeed: 100,
+      showCursor: true,
+      strings: ["Developer", "Designer", "Music Producer", "Content Creator"],
+    });
+  }, []);
+  return (
+    <div className="intro" id="intro">
+      <div className="left">
+        <div className="imgContainer">
+          <img src="assets/me-1.png" alt="" />
+        </div>
+      </div>
+      <div className="right">
+        <div className="wrapper">
+          <h2>Hello There! I'm </h2>
+          <h1>Randy Anderson</h1>
+          <h3>
+            I'm a <span ref={textRef}></span>
+          </h3>
+        </div>
+        <a href="#portfolio">
+          <img src="assets/down.png" alt="" />
+        </a>
+      </div>
+    </div>
+  );
 }
