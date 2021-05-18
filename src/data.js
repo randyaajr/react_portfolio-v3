69f64d67df68cd75a20ee097bc1014ecc51a33df
randyaajr <67880000+randyaajr@users.noreply.github.com>
2021-05-18 17:42:54 -0400

    Added two more projects for testing

--- src/data.js
+++ src/data.js
@@ -1,19 +1,44 @@
 export const featuredPortfolio = [{
-    id: 1,
-    title: "E-commerce",
-    img: "assets/boutique.PNG",
-    link: "https://randyaajr-shop.herokuapp.com/",
-}, ];
+        id: 1,
+        title: "E-commerce",
+        img: "assets/boutique.PNG",
+        link: "https://randyaajr-shop.herokuapp.com/",
+    },
+    {
+        id: 1,
+        title: "STACKEM",
+        img: "assets/stackem.PNG",
+        link: "https://randyaajr.github.io/javascript_stack-em-game/",
+    },
+    {
+        id: 1,
+        title: "Love Runnings",
+        img: "assets/loverunnings0.PNG",
+        link: "https://randyaajr.github.io/Love-Runnings/",
+    },
+];
 
 export const webPortfolio = [{
-    id: 1,
-    title: "Love Runnings",
-    img: "assets/loverunnings0.PNG",
-    link: "https://randyaajr.github.io/Love-Runnings/",
-}, ];
+        id: 1,
+        title: "E-commerce",
+        img: "assets/boutique.PNG",
+        link: "https://randyaajr-shop.herokuapp.com/",
+    },
+    {
+        id: 1,
+        title: "Love Runnings",
+        img: "assets/loverunnings0.PNG",
+        link: "https://randyaajr.github.io/Love-Runnings/",
+    },
+];
 
 export const mobilePortfolio = [];
 
-export const gamesPortfolio = [];
+export const gamesPortfolio = [{
+    id: 1,
+    title: "STACKEM",
+    img: "assets/stackem.PNG",
+    link: "https://randyaajr.github.io/javascript_stack-em-game/",
+}, ];
 
 export const contentPortfolio = [];
