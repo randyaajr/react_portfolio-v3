1fb446452957c38ca1c1b0e6a73aad38d85083c8
randyaajr <67880000+randyaajr@users.noreply.github.com>
2021-05-20 14:08:34 -0400

    Updated with current programming projects

--- src/data.js
+++ src/data.js
@@ -5,16 +5,100 @@ export const featuredPortfolio = [{
         link: "https://randyaajr-shop.herokuapp.com/",
     },
     {
-        id: 1,
+        id: 2,
         title: "STACKEM",
         img: "assets/stackem.PNG",
         link: "https://randyaajr.github.io/javascript_stack-em-game/",
     },
     {
-        id: 1,
+        id: 3,
         title: "Love Runnings",
         img: "assets/loverunnings0.PNG",
         link: "https://randyaajr.github.io/Love-Runnings/",
+    },
+    {
+        id: 4,
+        title: "Task Manager",
+        img: "assets/task-manager0.PNG",
+        link: "https://task-manager-randy.herokuapp.com/get_tasks",
+    },
+    {
+        id: 5,
+        title: "Portfolio V1",
+        img: "assets/resume0.PNG",
+        link: "https://randyaajr.github.io/resume-webpage/",
+    },
+    {
+        id: 6,
+        title: "Portfolio V2",
+        img: "assets/portfolio-old.PNG",
+        link: "https://randyaajr.github.io/portfolio-randy/",
+    },
+    {
+        id: 7,
+        title: "InfoFlix",
+        img: "assets/infoflix0.PNG",
+        link: "https://randyaajr.github.io/InfoFlix_Milestone-2/",
+    },
+    {
+        id: 8,
+        title: "Meal Maid",
+        img: "assets/meal-maid0.PNG",
+        link: "https://randyaajr.github.io/Meal-Maid_Milestone-1/",
+    },
+    {
+        id: 9,
+        title: "Whisky Drop",
+        img: "assets/whiskydrop0.PNG",
+        link: "https://randyaajr.github.io/bootstrap-whiskeydrop-first-repo/",
+    },
+    {
+        id: 10,
+        title: "Todo-List",
+        img: "assets/todo-app0.PNG",
+        link: "https://randyaajr.github.io/Todo-list/",
+    },
+    {
+        id: 11,
+        title: "Boxie",
+        img: "assets/Boxie-game0.PNG",
+        link: "https://randyaajr.github.io/Javascript-box-game/",
+    },
+    {
+        id: 12,
+        title: "Space Invaders",
+        img: "assets/sp-inv.PNG",
+        link: "https://codepen.io/randyaajr/full/ExgeReq",
+    },
+    {
+        id: 13,
+        title: "Light Up Icon Buttons",
+        img: "assets/light-up.PNG",
+        link: "https://codepen.io/randyaajr/full/XWjYaJq",
+    },
+    {
+        id: 14,
+        title: "Tetris",
+        img: "assets/tetris.PNG",
+        link: "https://codepen.io/randyaajr/full/abmaKXb",
+    },
+    {
+        id: 15,
+        title: "Mr. Floatie",
+        img: "assets/floaty-ghost.PNG",
+        link: "https://codepen.io/randyaajr/full/ExgRvyd",
+    },
+    {
+        id: 16,
+        title: "Archery",
+        img: "assets/archery.PNG",
+        link: "https://codepen.io/randyaajr/full/LYRrjRv",
+    },
+    {
+        id: 17,
+        title: "Space Dots",
+        img: "assets/sdg.PNG",
+        link: "https://codepen.io/randyaajr/full/xxEzrKr",
     },
 ];
 
@@ -25,20 +109,93 @@ export const webPortfolio = [{
         link: "https://randyaajr-shop.herokuapp.com/",
     },
     {
-        id: 1,
+        id: 2,
         title: "Love Runnings",
         img: "assets/loverunnings0.PNG",
         link: "https://randyaajr.github.io/Love-Runnings/",
+    },
+    {
+        id: 3,
+        title: "Task Manager",
+        img: "assets/task-manager0.PNG",
+        link: "https://task-manager-randy.herokuapp.com/get_tasks",
+    },
+    {
+        id: 4,
+        title: "Portfolio V1",
+        img: "assets/resume0.PNG",
+        link: "https://randyaajr.github.io/resume-webpage/",
+    },
+    {
+        id: 5,
+        title: "Portfolio V2",
+        img: "assets/portfolio-old.PNG",
+        link: "https://randyaajr.github.io/portfolio-randy/",
+    },
+    {
+        id: 6,
+        title: "InfoFlix",
+        img: "assets/infoflix0.PNG",
+        link: "https://randyaajr.github.io/InfoFlix_Milestone-2/",
+    },
+    {
+        id: 7,
+        title: "Meal Maid",
+        img: "assets/meal-maid0.PNG",
+        link: "https://randyaajr.github.io/Meal-Maid_Milestone-1/",
+    },
+    {
+        id: 8,
+        title: "Whisky Drop",
+        img: "assets/whiskydrop0.PNG",
+        link: "https://randyaajr.github.io/bootstrap-whiskeydrop-first-repo/",
+    },
+    {
+        id: 9,
+        title: "Todo-List",
+        img: "assets/todo-app0.PNG",
+        link: "https://randyaajr.github.io/Todo-list/",
     },
 ];
 
 export const mobilePortfolio = [];
 
 export const gamesPortfolio = [{
-    id: 1,
-    title: "STACKEM",
-    img: "assets/stackem.PNG",
-    link: "https://randyaajr.github.io/javascript_stack-em-game/",
-}, ];
+        id: 1,
+        title: "Space Dots",
+        img: "assets/sdg.PNG",
+        link: "https://codepen.io/randyaajr/full/xxEzrKr",
+    },
+    {
+        id: 2,
+        title: "STACKEM",
+        img: "assets/stackem.PNG",
+        link: "https://randyaajr.github.io/javascript_stack-em-game/",
+    },
+    {
+        id: 3,
+        title: "Archery",
+        img: "assets/archery.PNG",
+        link: "https://codepen.io/randyaajr/full/LYRrjRv",
+    },
+    {
+        id: 4,
+        title: "Tetris",
+        img: "assets/tetris.PNG",
+        link: "https://codepen.io/randyaajr/full/abmaKXb",
+    },
+    {
+        id: 5,
+        title: "Space Invaders",
+        img: "assets/sp-inv.PNG",
+        link: "https://codepen.io/randyaajr/full/ExgeReq",
+    },
+    {
+        id: 6,
+        title: "Boxie",
+        img: "assets/Boxie-game0.PNG",
+        link: "https://randyaajr.github.io/Javascript-box-game/",
+    },
+];
 
 export const contentPortfolio = [];
