4de0a08451425892a0ced69f869c9d8d5c8b89db
randyaajr <67880000+randyaajr@users.noreply.github.com>
2021-08-21 17:22:10 -0400

    Contact page created with usestate

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,5 +1,27 @@
+import { useState } from "react";
 import "./Contact.scss";
 
 export default function Contact() {
-  return <div className="contact" id="contact"></div>;
+  const [message, setMessage] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setMessage(true);
+  };
+  return (
+    <div className="contact" id="contact">
+      <div className="left">
+        <img src="assets/shake.svg" alt="" />
+      </div>
+      <div className="right">
+        <h2>Contact Me</h2>
+        <form onSubmit={handleSubmit}>
+          <input type="text" placeholder="Email" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$" required />
+          <textarea placeholder="Message" maxLength="1000" required></textarea>
+          <button type="submit">Send</button>
+          {message && <span>Thank you for reaching out, I'll reply ASAP! 👍</span>}
+        </form>
+      </div>
+    </div>
+  );
 }
